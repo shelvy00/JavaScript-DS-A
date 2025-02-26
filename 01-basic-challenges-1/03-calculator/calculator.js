461584e74b88db7a33d7532b69d883f625a14ebb
Shelvy <shelvy511@gmail.com>
2025-02-25 22:45:44 -0600

    update

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,19 @@
-function calculator() {}
+function calculator(num1, num2, op) {
+  if (op === "+") {
+    return num1 + num2;
+  }
+
+  if (op === "-") {
+    return num1 - num2;
+  }
+
+  if (op === "*") {
+    return num1 * num2;
+  }
+
+  if (op === "/") {
+    return num1 / num2;
+  }
+}
 
 module.exports = calculator;
