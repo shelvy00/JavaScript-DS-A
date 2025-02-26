48a8f5ed575e82dada00afc200f5f6f47e5a3842
Shelvy <shelvy511@gmail.com>
2025-02-26 01:05:55 -0600

    update

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,12 @@
-function countOccurrences() {}
+function countOccurrences(str, char) {
+  let results = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i].includes(char)) {
+      results++;
+    }
+  }
+  return results;
+}
 
 module.exports = countOccurrences;
