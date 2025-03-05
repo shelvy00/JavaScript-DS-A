e00a46542aec2c3cbd2e94795c9c69ba3aeed497
Shelvy <shelvy511@gmail.com>
2025-03-05 15:52:45 -0600

    completed title-case

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,9 @@
-function titleCase() {}
+function titleCase(str) {
+  return str
+    .toLowerCase() // Ensure the string is in lowercase first
+    .split(" ") // Split the string into words
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1)) // Capitalize the first letter of each word
+    .join(" "); // Join the words back into a string
+}
 
 module.exports = titleCase;
