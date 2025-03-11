3571fbbde20844c6b3ad3995cc684796a5751921
Shelvy <shelvy511@gmail.com>
2025-03-10 20:57:52 -0500

    update count-vowels

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,40 @@
-function countVowels() {}
+function countVowels(str) {
+  //   My way
+
+  //     let results = 0;
+  //   let vowels = "a,e,i,o,u";
+
+  //   for (let i = 0; i < str.length; i++) {
+  //     if (vowels.includes(str[i].toLowerCase())) {
+  //       results++;
+  //     }
+  //   }
+  //   return results;
+
+  // Declare a variable to store the formatted string
+  const formattedStr = str.toLowerCase();
+  // Declare a variable to store the number of vowels
+  let count = 0;
+
+  // Loop through the string
+  for (let i = 0; i < formattedStr.length; i++) {
+    // Declare a variable to store the current character
+    const char = formattedStr[i];
+
+    // If the current character is a vowel, increment the count
+    if (
+      char === "a" ||
+      char === "e" ||
+      char === "i" ||
+      char === "o" ||
+      char === "u"
+    ) {
+      count++;
+    }
+  }
+
+  // Return the number of vowels
+  return count;
+}
 
 module.exports = countVowels;
