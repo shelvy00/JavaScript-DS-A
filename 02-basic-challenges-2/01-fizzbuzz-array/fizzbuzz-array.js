6a10b7b8744969b1871d5aff2a45e6e0363857c6
Shelvy <shelvy511@gmail.com>
2025-03-14 19:45:00 -0500

    fizz-buzz

--- 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
+++ 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
@@ -1,3 +1,18 @@
-function fizzBuzzArray() {}
+function fizzBuzzArray(num) {
+  let results = [];
+
+  for (i = 1; i <= num; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      results.push("FizzBuzz");
+    } else if (i % 3 === 0) {
+      results.push("Fizz");
+    } else if (i % 5 === 0) {
+      results.push("Buzz");
+    } else {
+      results.push(i);
+    }
+  }
+  return results;
+}
 
 module.exports = fizzBuzzArray;
