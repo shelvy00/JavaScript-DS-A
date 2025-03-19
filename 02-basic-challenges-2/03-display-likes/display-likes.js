d083bc05b2c604965b85c096a5fe37e5ec6f4ce4
Shelvy <shelvy511@gmail.com>
2025-03-19 01:46:01 -0500

    display-likes

--- 02-basic-challenges-2/03-display-likes/display-likes.js
+++ 02-basic-challenges-2/03-display-likes/display-likes.js
@@ -1,3 +1,15 @@
-function displayLikes() {}
+function displayLikes(arr) {
+  if (arr.length === 0) {
+    return "no one likes this";
+  } else if (arr.length === 1) {
+    return `${arr} likes this`;
+  } else if (arr.length === 2) {
+    return `${arr[0]} and ${arr[1]} like this`;
+  } else if (arr.length === 3) {
+    return `${arr[0]}, ${arr[1]} and ${arr[2]} like this`;
+  } else if (arr.length > 3) {
+    return `${arr[0]}, ${arr[1]} and ${[arr.length - 2]} others like this`;
+  }
+}
 
 module.exports = displayLikes;
