dd25b212ab16cf2a73f305939b02e87aa7eb7e7a
Shelvy <shelvy511@gmail.com>
2025-03-22 18:09:39 -0500

    find missing number

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,3 +1,20 @@
-function findMissingNumber() {}
+function findMissingNumber(arr) {
+  // If the array is empty or undefined, return undefined
+  if (!arr || arr.length === 0) {
+    return undefined;
+  }
+
+  // Length of the array
+  const n = arr.length + 1;
+
+  // Calculate the sum of first n natural numbers using the formula: n * (n + 1) / 2
+  const expectedSum = (n * (n + 1)) / 2;
+
+  // Calculate the sum of numbers in the array
+  const actualSum = arr.reduce((acc, num) => acc + num, 0);
+
+  // The missing number is the difference between the expected sum and actual sum
+  return expectedSum - actualSum;
+}
 
 module.exports = findMissingNumber;
