c15dde59814a2fa0d4881da8fea33845c2107b06
Shelvy <shelvy511@gmail.com>
2025-03-23 13:56:14 -0500

    find-missing-letter

--- 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
+++ 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
@@ -1,3 +1,18 @@
-function findMissingLetter() {}
+function findMissingLetter(arr) {
+  if (!arr || arr.length === 0) {
+    return undefined;
+  }
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    // Compare the ASCII values of consecutive characters
+    if (arr[i + 1].charCodeAt(0) - arr[i].charCodeAt(0) > 1) {
+      // Return the missing letter by incrementing the current letter's ASCII value
+      return String.fromCharCode(arr[i].charCodeAt(0) + 1);
+    }
+  }
+
+  // Return undefined if no letter is missing
+  return undefined;
+}
 
 module.exports = findMissingLetter;
