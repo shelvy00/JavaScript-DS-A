5b948c1f423735367b0fd5bf4a572efa3e7ef12d
Shelvy <shelvy511@gmail.com>
2025-03-26 22:21:49 -0500

    are-all-chars-unique

--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -1,3 +1,9 @@
-function areAllCharactersUnique() {}
+function areAllCharactersUnique(str) {
+  // Create a Set from the string characters
+  const charSet = new Set(str);
+
+  // Compare the size of the Set with the length of the string
+  return charSet.size === str.length;
+}
 
 module.exports = areAllCharactersUnique;
