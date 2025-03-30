0256f3431f1535a80841acb764c04c26904851c1
Shelvy <shelvy511@gmail.com>
2025-03-30 18:19:24 -0500

    updated

--- 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
+++ 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
@@ -1,3 +1,20 @@
-function findFirstNonRepeatingCharacter() {}
+function findFirstNonRepeatingCharacter(str) {
+  const charCount = {};
+
+  // Count the occurrences of each character
+  for (const char of str) {
+    charCount[char] = (charCount[char] || 0) + 1;
+  }
+
+  // Find the first non-repeating character
+  for (const char of str) {
+    if (charCount[char] === 1) {
+      return char;
+    }
+  }
+
+  // If no non-repeating character is found, return null
+  return null;
+}
 
 module.exports = findFirstNonRepeatingCharacter;
