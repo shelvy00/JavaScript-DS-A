206941b887ccef6f7d5f62fb3e873705bb97b7b7
Shelvy <shelvy511@gmail.com>
2025-04-06 19:02:40 -0500

    format-phone-number

--- 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
+++ 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
@@ -1,3 +1,19 @@
-function formatPhoneNumber() {}
+function formatPhoneNumber(num) {
+  // Remove any non-digit characters
+  const cleaned = ("" + num).replace(/\D/g, "");
+
+  // Check if it has exactly 10 digits
+  if (cleaned.length !== 10) {
+    return "Invalid phone number";
+  }
+
+  // Format and return (123) 456-7890
+  const match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/);
+  if (match) {
+    return `(${match[1]}) ${match[2]}-${match[3]}`;
+  }
+
+  return "Invalid phone number";
+}
 
 module.exports = formatPhoneNumber;
