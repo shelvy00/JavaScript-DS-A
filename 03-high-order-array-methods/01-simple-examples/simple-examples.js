efa64c07bcdb5086cc4db8a6228c377c3f1e817b
Shelvy <shelvy511@gmail.com>
2025-04-17 21:55:43 -0500

    update

--- 03-high-order-array-methods/01-simple-examples/simple-examples.js
+++ 03-high-order-array-methods/01-simple-examples/simple-examples.js
@@ -13,11 +13,16 @@ const evenNumbers = numbers.filter((num) => num > 2);
 /**
  * reduce: Accumulates array elements into a single value using a provided function.
  */
-
+const addNumbers = numbers.reduce((total, num) => {
+  return total + num;
+}, 0);
+console.log(addNumbers);
 /**
  * forEach: Iterates through array elements and applies a function without creating a new array.
  */
-
+const newArr = numbers.forEach((number) => {
+  console.log(number);
+});
 /**
  * find: Returns the first array element that satisfies a specified condition.
  */
