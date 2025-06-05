3c68498c017ad937a2b8599a09d14819cd10d117
Shelvy <shelvy511@gmail.com>
2025-06-04 23:13:13 -0500

    calculate-total-sales

--- 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
+++ 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
@@ -1,3 +1,14 @@
-function calculateTotalSalesWithTax() {}
+function calculateTotalSalesWithTax(products, tax) {
+  const totalSales = products.reduce(
+    (sum, product) => sum + product.price * product.quantity,
+    0
+  );
+
+  const taxAmount = (totalSales * tax) / 100;
+
+  const totalSalesWithTax = totalSales + taxAmount;
+
+  return parseFloat(totalSalesWithTax.toFixed(2));
+}
 
 module.exports = calculateTotalSalesWithTax;
