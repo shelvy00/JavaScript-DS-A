4231782428ac46a36c8eddd43245194ca4625aa4
Shelvy <shelvy511@gmail.com>
2025-06-05 22:51:00 -0500

    highest-scoring-word

--- 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word-run.js
+++ 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word-run.js
@@ -1,5 +1,5 @@
-const highestScoringWord = require('./highest-scoring-word');
+const highestScoringWord = require("./highest-scoring-word");
 
-const result = highestScoringWord('Hello my name is xavier');
+const result = highestScoringWord("what time are we climbing up the volcano");
 
 console.log(result);
--- 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
+++ 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
@@ -1,3 +1,22 @@
-function highestScoringWord() {}
+function highestScoringWord(str) {
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+  const words = str.split(" ");
+
+  let highestScore = 0;
+  let highestWord = "";
+
+  for (const word of words) {
+    const score = [...word.toLowerCase()].reduce((sum, char) => {
+      return sum + (alphabet.indexOf(char) + 1);
+    }, 0);
+
+    if (score > highestScore) {
+      highestScore = score;
+      highestWord = word;
+    }
+  }
+
+  return highestWord;
+}
 
 module.exports = highestScoringWord;
