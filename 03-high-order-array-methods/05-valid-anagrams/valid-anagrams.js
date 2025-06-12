abd24768822e1ef6ba242885b590ed40c5dc2504
Shelvy <shelvy511@gmail.com>
2025-06-11 21:34:00 -0500

    valid-anagrams

--- 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
+++ 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
@@ -1,3 +1,20 @@
-function validAnagrams() {}
+function validAnagrams(str1, str2) {
+  if (str1.length !== str2.length) return false;
+
+  const charCount = (arr) =>
+    arr.reduce((acc, char) => {
+      acc[char] = (acc[char] || 0) + 1;
+      return acc;
+    }, {});
+
+  const freq1 = charCount([...str1]);
+  const freq2 = charCount([...str2]);
+
+  for (let char in freq1) {
+    if (freq1[char] !== freq2[char]) return false;
+  }
+
+  return true;
+}
 
 module.exports = validAnagrams;
